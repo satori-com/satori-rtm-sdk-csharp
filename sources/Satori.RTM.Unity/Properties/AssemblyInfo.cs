18a514683b31453b58c5f9830d8e07d7c9471729
Michael Kuzmenkov <mkuzmenkov@satori.com>
2017-05-10 00:25:16 +0200

    Fill copyright info; Remove dependency on UnityEngine.dll; Rename AssemblyVersin to AssemblyVersion; Add linker config file for Unity/iOS build

--- sources/Satori.RTM.Unity/Properties/AssemblyInfo.cs
+++ sources/Satori.RTM.Unity/Properties/AssemblyInfo.cs
@@ -7,20 +7,11 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Satori.RTM.Unity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Satori Worldwide, Inc.")]
+[assembly: AssemblyProduct("Satori.RTM.Unity")]
+[assembly: AssemblyCopyright("Copyright (c) 2017, Satori Worldwide, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+[assembly: InternalsVisibleTo("Satori.RTM.Test.Unity")]
 
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
