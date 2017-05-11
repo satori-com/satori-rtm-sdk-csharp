34b6bf6747dec58a1c7aced0c44d55812de15d45
Michael Kuzmenkov <mkuzmenkov@satori.com>
2017-05-11 20:34:30 +0200

    Add test project stub for Unity

---
+++ tests/integration/Satori.RTM.Test.Unity/UnityTestRunner.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Satori.RTM.Test.Unity
+{
+    public class UnityTestRunner
+    {
+        public static void Run()
+        {
+            //this is needed only once
+            //CoreExtensions.Host.InstallBuiltins();
+
+            //TestStatus suit = new TestSuite(new NullListener(), TestFilter.Empty);
+        }
+    }
+}
