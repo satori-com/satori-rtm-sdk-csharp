ebb869a70849e52459af6cc80a5455b1c69ffd34
Michael Kuzmenkov <mkuzmenkov@satori.com>
2017-05-17 21:42:24 +0200

    Refactor HelloWorld project for .NET 4.5

--- examples/HelloWorld.Net45/App.cs
+++ examples/HelloWorld.Net45/App.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Satori.Rtm;
 using Satori.Rtm.Client;
 
 namespace HelloWorld
 {
     class App
     {
+
+        // Sample model to publish to RTM.
+        // This class represents the following raw json structure:
+        // {
+        //   "who": "zebra",
+        //  "where": [34.134358,-118.321506]
+        // }
+        class Animal
+        {
+            [JsonProperty("who")]
+            public string Who { get; set; }
+
+            [JsonProperty("where")]
+            public float[] Where { get; set; }
+        }
+
         static void Main(string[] args)
         {
+            // Log messages from SDK to the console
+            Trace.Listeners.Add(new ConsoleTraceListener());
+
+            // Change logging levels to increase verbosity. Default level is Warning. 
+            DefaultLoggers.Dispatcher.SetLevel(Logger.LogLevel.Warning);
+            DefaultLoggers.Serialization.SetLevel(Logger.LogLevel.Warning);
+            DefaultLoggers.Connection.SetLevel(Logger.LogLevel.Warning);
+            DefaultLoggers.Client.SetLevel(Logger.LogLevel.Warning);
+            DefaultLoggers.ClientRtm.SetLevel(Logger.LogLevel.Warning);
+            DefaultLoggers.ClientRtmSubscription.SetLevel(Logger.LogLevel.Warning); 
+
             if (args.Length != 2)
             {
                 Console.WriteLine("Enter parameters: <endpoint> <app key>");
@@ -16,44 +48,79 @@ namespace HelloWorld
             var endpoint = args[0];
             var appKey = args[1];
 
-            // create client
-            var client = new RtmClientBuilder(endpoint, appKey).Build();
+            IRtmClient client = new RtmClientBuilder(endpoint, appKey).Build();
 
-            Start(client);
+            // Hook up to client lifecycle events 
+            client.OnEnterConnecting += () => Console.WriteLine("Connecting...");
+            client.OnEnterConnected += cn => Console.WriteLine("Connected");
+            client.OnLeaveConnected += cn => Console.WriteLine("Disconnected");
+            client.OnError += ex => Console.Error.WriteLine("Error occurred: " + ex.Message);
 
-            Console.ReadKey(true);
+            client.Start();
+
+            // This event will be signalled once published message is received
+            var ev = new ManualResetEvent(initialState: false);
+
+            RunAsync(client, ev);
+
+            ev.WaitOne();
+
+            // Dispose the client before exiting the app
             client.Dispose().Wait();
         }
 
-        public static async void Start(IRtmClient client)
+        private static async void RunAsync(IRtmClient client, ManualResetEvent ev)
         {
-            var channel = $"{Guid.NewGuid():N}";
+            var channel = "my_channel";
 
-            // handler for subscription channel data
+            // Create subscription observer to observe channel subscription events 
             var observer = new SubscriptionObserver();
-            observer.OnSubscriptionData += (subscription, data) =>
+
+            observer.OnEnterSubscribed += sub =>
             {
-                // print messages
-                foreach (var m in data.Messages)
+                Console.WriteLine("Client subscribed to " + sub.SubscriptionId);
+
+                var msg = new Animal
                 {
-                    Console.WriteLine(m);
-                }
+                    Who = "zebra",
+                    Where = new float[] { 34.134358f, -118.321506f }
+                };
+
+                // Publish message to the subscribed channel
+                client.Publish(channel, msg, Ack.Yes)
+                    .ContinueWith(t =>
+                    {
+                        if (t.Exception == null)
+                            Console.WriteLine("Published successfully!");
+                        else
+                            Console.Error.WriteLine("Publishing failed: " + t.Exception);
+                    });
             };
 
-            // run client
-            await client.Start();
+            observer.OnLeaveSubscribed += sub 
+                => Console.WriteLine("Unsubscribed from " + sub.SubscriptionId);
 
-            // publish message and remember its position
-            var reply = await client.Publish(channel, "hello world!", Ack.Yes);
+            observer.OnSubscriptionError += (ISubscription sub, RtmSubscriptionError err) 
+                => Console.Error.WriteLine("Subscription error " + err.Code + ": " + err.Reason);
 
-            // create subscription from remembered position
-            await client.CreateSubscription(
-                channel,
-                new SubscriptionConfig(SubscriptionModes.Advanced)
+            observer.OnSubscriptionData += (ISubscription sub, RtmSubscriptionData data) =>
+            {
+                Console.WriteLine("Message received");
+
+                foreach(JToken token in data.Messages) 
                 {
-                    Position = reply.Position,
-                    Observer = observer
-                });
+                    Animal msg = token.ToObject<Animal>();
+                    Console.WriteLine("Hello {0} at ({1})!", msg.Who, string.Join(", ", msg.Where));
+                }
+
+                // Signal the manual reset event to exit the app
+                ev.Set();
+            };
+
+            // Subscribe to the channel. Because client is not connected to Satori RTM yet, 
+            // subscription request will be queued. This request will be sent when 
+            // the client is connected. 
+            await client.CreateSubscription(channel, SubscriptionModes.Simple, observer);
         }
     }
 }
