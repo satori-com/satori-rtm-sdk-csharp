3dc13b4436972b09b55ab4a729f0ae2cf9b67d60
Michael Kuzmenkov <mkuzmenkov@satori.com>
2017-06-14 17:28:50 +0200

    Snippets for publishing and subscribing

---
+++ examples/Publishing.Net45/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Satori.Rtm;
+using Satori.Rtm.Client;
+
+namespace Publishing
+{
+    class Program
+    {
+        const string endpoint = "YOUR_ENDPOINT";
+        const string appkey = "YOUR_APPKEY";
+
+        const string channel = "animals";
+
+        class Animal
+        {
+            [JsonProperty("who")]
+            public string Who { get; set; }
+
+            [JsonProperty("where")]
+            public float[] Where { get; set; }
+        }
+
+        static void Main()
+        {
+            // Log messages from SDK to the console
+            Trace.Listeners.Add(new ConsoleTraceListener());
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync() 
+        {
+            IRtmClient client = new RtmClientBuilder(endpoint, appkey)
+                .Build();
+
+            try 
+            {
+                var message = new Animal 
+                {
+                    Who = "zebra",
+                    Where =  new float[] {34.134358f, -118.321506f}
+                };
+
+                RtmPublishReply reply = await client.Publish(channel, message, Ack.Yes);
+            }
+            catch(PduException ex) 
+            {
+                Console.WriteLine("Publishing failed because RTM replied with the error {0}: {1}", ex.Error.Code, ex.Error.Reason);
+            }
+            catch(DisconnectedException ex)
+            {
+                Console.WriteLine("Publishing failed because the client was disconnected: " + ex.Message);
+            }
+            catch(TooManyRequestsException ex)
+            {
+                Console.WriteLine("Publishing failed because the client's offline action queue exceeds RtmClientBuilder.PendingActionQueueLength");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Publishing failed: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ examples/SubscriptionWithFilter.Net45/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Satori.Rtm;
+using Satori.Rtm.Client;
+
+namespace SubscriptionWithFilter
+{
+    class Program
+    {
+        const string endpoint = "YOUR_ENDPOINT";
+        const string appkey = "YOUR_APPKEY";
+
+        const string channel = "animals";
+
+        class Animal
+        {
+            [JsonProperty("who")]
+            public string Who { get; set; }
+
+            [JsonProperty("where")]
+            public float[] Where { get; set; }
+        }
+
+        static void Main()
+        {
+            // Log messages from SDK to the console
+            Trace.Listeners.Add(new ConsoleTraceListener());
+
+            IRtmClient client = new RtmClientBuilder(endpoint, appkey)
+                .Build();
+
+            var observer = new SubscriptionObserver();
+
+            observer.OnEnterSubscribed += (ISubscription sub) => 
+                Console.WriteLine("Subscribed to: " + sub.SubscriptionId);
+
+            observer.OnSubscriptionData += (ISubscription sub, RtmSubscriptionData data) => 
+            {
+                foreach(JToken jToken in data.Messages)
+                {
+                    Animal msg = jToken.ToObject<Animal>();
+                    string text = string.Format("Who? {0}. Where? At {1},{2}", msg.Who, msg.Where[0], msg.Where[1]);
+                    Console.WriteLine("Got message: " + text);
+                }
+            };
+
+            observer.OnSubscribeError += (ISubscription sub, Exception err) => 
+            {
+                var rtmEx = err as RtmSubscribeException;
+                if (rtmEx != null) 
+                    Console.WriteLine("Subscribing failed because RTM replied with the error {0}: {1}", rtmEx.Error.Code, rtmEx.Error.Reason);
+                else 
+                    Console.WriteLine("Subscribing failed: " + err.Message);
+            };
+
+            observer.OnSubscriptionError += (ISubscription sub, RtmSubscriptionError err) => 
+                Console.WriteLine("Subscription failed because RTM sent the error {0}: {1}", err.Code, err.Reason);
+
+            // Assume that someone already publishes animals to the channel 'animals'
+            var cfg = new SubscriptionConfig(SubscriptionModes.Simple, observer)
+            {
+                Filter = $"SELECT * FROM {channel} WHERE who = 'zebra'"
+            };
+            client.CreateSubscription("zebras", cfg);
+
+            // Do not exit the program
+            new ManualResetEvent(false).WaitOne();
+        }
+    }
+}
