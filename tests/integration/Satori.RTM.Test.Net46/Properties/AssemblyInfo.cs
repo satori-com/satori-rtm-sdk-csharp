3ef1370880ac4cb5212b4d5481ddd2fc4d2acac6
Michael Kuzmenkov <mkuzmenkov@satori.com>
2017-07-06 15:34:08 +0200

    Fix .NETStandard tests

--- tests/integration/Satori.RTM.Test.Net46/Properties/AssemblyInfo.cs
+++ tests/integration/Satori.RTM.Test.Net46/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("87b238e0-6674-46c4-8264-b582ab8610ec")]
+[assembly: Guid("19ec6ad1-ddaf-4dbd-8c71-2c02342039d4")]
 
 // Version information for an assembly consists of the following four values:
 //
