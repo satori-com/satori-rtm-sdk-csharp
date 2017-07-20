02be72b950c647ea3b35bd2a877a1ea2b9028e79
Michael Kuzmenkov <mkuzmenkov@satori.com>
2017-07-21 01:24:30 +0200

    Add SubscribeToOpenChannel example

---
+++ examples/net/SubscribeToOpenChannel.Net45/SubscribeToOpenChannel.g.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Satori.Rtm;
+using Satori.Rtm.Client;
+
+class Program
+{
+    const string endpoint = "YOUR_ENDPOINT";
+    const string appkey = "YOUR_APPKEY";
+    const string channel = "OPEN_CHANNEL";
+
+    static void Main()
+    {
+        // Log messages from SDK to the console
+        Trace.Listeners.Add(new ConsoleTraceListener());
+
+        IRtmClient client = new RtmClientBuilder(endpoint, appkey).Build();
+        client.OnEnterConnected += cn => Console.WriteLine("Connected to Satori RTM!");
+        client.Start();
+
+        // Create subscription observer to observe channel subscription events 
+        var observer = new SubscriptionObserver();
+
+        observer.OnSubscriptionData += (ISubscription sub, RtmSubscriptionData data) => 
+        {
+            foreach(JToken jToken in data.Messages)
+            {
+                Console.WriteLine("Got message: " + jToken);
+            }
+        };
+        
+         client.CreateSubscription(channel, SubscriptionModes.Simple, observer);
+
+        Console.ReadKey();
+
+        // Stop and clean up the client before exiting the program
+        client.Dispose().Wait();
+    }
+}
