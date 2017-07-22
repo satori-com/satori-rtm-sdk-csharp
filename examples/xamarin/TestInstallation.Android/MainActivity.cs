084e2d98fb78bb92db81a6966c85f4532efd8111
Michael Kuzmenkov <mkuzmenkov@satori.com>
2017-07-22 05:38:38 +0200

    Polish TestInstallation.Android example for Xamarin

--- examples/xamarin/TestInstallation.Android/MainActivity.cs
+++ examples/xamarin/TestInstallation.Android/MainActivity.cs
@@ -3,38 +3,32 @@ using Android.Widget;
 using Android.OS;
 using Android.Util;
 using Satori.Rtm.Client;
+using TestInstallation;
 
-namespace TestInstallation
+[Activity(Label = "TestInstallation", MainLauncher = true, Icon = "@drawable/icon")]
+public class MainActivity : Activity
 {
+    const string endpoint = "YOUR_ENDPOINT";
+    const string appkey = "YOUR_APPKeY";
 
-    [Activity(Label = "TestInstallation", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    protected override void OnCreate(Bundle bundle)
     {
-        const string endpoint = "YOUR_ENDPOINT";
-        const string appkey = "YOUR_APPKeY";
+        base.OnCreate(bundle);
 
-        const string tag = "TestInstallation";
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.Main);
 
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
+        IRtmClient client = new RtmClientBuilder(endpoint, appkey).Build();
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+        client.OnEnterConnected += cn 
+            => Log.Info("", "Connected to Satori RTM!");
 
-            IRtmClient client = new RtmClientBuilder(endpoint, appkey).Build();
+        client.OnError += ex =>
+            Log.Error("", "Failed to connect: " + ex.Message);
 
-            client.OnEnterConnected += cn 
-                => Log.Info(tag, "Connected to Satori RTM!");
+        client.Start();
 
-            client.OnError += ex =>
-                Log.Error(tag, "Failed to connect: " + ex.Message);
+        // Publish, subscribe, and perform other operations here
 
-            client.Start();
-
-            // Publish, subscribe, and perform other operations here
-
-        }
     }
-
 }
