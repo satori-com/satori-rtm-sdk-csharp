6f80103938c178ec406c7cbd605fa7f053fdaccd
Michael Kuzmenkov <mkuzmenkov@satori.com>
2017-07-22 03:51:07 +0200

    Add TestInstallation.iOS example for Xamarin

---
+++ examples/xamarin/TestInstallation.iOS/AppDelegate.cs
@@ -0,0 +1,77 @@
+﻿using Foundation;
+using Satori.Rtm.Client;
+using System;
+using UIKit;
+
+namespace TestInstallation
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        const string endpoint = "YOUR_ENDPOINT";
+        const string appkey = "YOUR_APPKeY";
+
+        public override UIWindow Window { get; set; }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            // create a new window instance based on the screen size
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            // If you have defined a root view controller, set it here:
+            // Window.RootViewController = myViewController;
+
+            // make the window visible
+            Window.MakeKeyAndVisible();
+
+            IRtmClient client = new RtmClientBuilder(endpoint, appkey).Build();
+
+            client.OnEnterConnected += cn
+                => Console.WriteLine("Connected to Satori RTM!");
+
+            client.OnError += ex =>
+                Console.WriteLine("Failed to connect: " + ex.Message);
+
+            client.Start();
+
+            // Publish, subscribe, and perform other operations here
+
+            return true;
+        }
+
+        public override void OnResignActivation(UIApplication application)
+        {
+            // Invoked when the application is about to move from active to inactive state.
+            // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
+            // or when the user quits the application and it begins the transition to the background state.
+            // Games should use this method to pause the game.
+        }
+
+        public override void DidEnterBackground(UIApplication application)
+        {
+            // Use this method to release shared resources, save user data, invalidate timers and store the application state.
+            // If your application supports background exection this method is called instead of WillTerminate when the user quits.
+        }
+
+        public override void WillEnterForeground(UIApplication application)
+        {
+            // Called as part of the transiton from background to active state.
+            // Here you can undo many of the changes made on entering the background.
+        }
+
+        public override void OnActivated(UIApplication application)
+        {
+            // Restart any tasks that were paused (or not yet started) while the application was inactive. 
+            // If the application was previously in the background, optionally refresh the user interface.
+        }
+
+        public override void WillTerminate(UIApplication application)
+        {
+            // Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
+        }
+    }
+}
+
+
